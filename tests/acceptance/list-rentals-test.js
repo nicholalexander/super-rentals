a8d6db7fc5fbf8568b90f1ef9238b8ef841cc6d8
nichol alexander <nichol.alexander@gmail.com>
2017-10-11 14:52:19 -0400

    some tests.

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -3,11 +3,28 @@ import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptan
 
 moduleForAcceptance('Acceptance | list rentals');
 
-test('should show rentals as the home page', function(assert){});
+test('should show rentals as the home page', function(assert){
+  visit('/');
+  andThen(function(){
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
+  });
+});
 
-test('should link to information about the company', function(assert){});
+test('should link to information about the company', function(assert){
+  visit('/');
+  click('a:contains("Contact")');
+  andThen(function() {
+    assert.equal(currentURL(), '/contact', 'should navigate to company contact page');
+  });
+});
 
-test('should link to contact information', function(assert){});
+test('should link to contact information', function(assert){
+  visit('/');
+  click('a:contains("About")');
+  andThen(function() {
+    assert.equal(currentURL(), '/about', 'should navigate to about us page');
+  });
+});
 
 test('should list available rentals', function(assert){});
 
