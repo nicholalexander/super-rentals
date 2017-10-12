32a6b37f3727fe595b744b890610aae0398a2d79
nichol alexander <nichol.alexander@gmail.com>
2017-10-12 14:12:51 -0400

    adapter and namespace.

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -29,7 +29,7 @@ test('should link to contact information', function(assert){
 test('should list available rentals', function(assert){
   visit('/');
   andThen(function(){
-    assert.equal(find('.listing').length, 3, 'should see 3 listings');
+    assert.equal(find('.listing').length, 3, 'should see 3 listings');  
   });
 });
 
